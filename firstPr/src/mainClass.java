1952f7491cf2510db705b57b89ed22041e94e27d
2012E1702003 <2012E1702003@muhlab30.lab.khas.edu.tr>
2012-09-25 07:43:54 -0430

    first lab

---
+++ firstPr/src/mainClass.java
@@ -0,0 +1,15 @@
+
+class mainClass {
+
+	public static void main(String[] args) {
+		int a;
+		a=5;
+		for(int i=1; i<a; i++){
+            System.out.println("Count is: "
+                               + i);
+       }
+
+
+	}
+
+}
